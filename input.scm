526f47e6f08c3f880e1e80629516aebbe9726a5a
novdelpap <35197388+novdelpap@users.noreply.github.com>
2025-06-11 15:17:10 -0600

    Update input.scm

--- input.scm
+++ input.scm
@@ -1,1 +1,1 @@
-(*local* "sdkpw" (ledger-set! (*state* documents arxiv the-synchronic-web) "0x116677764D9AADB6614F4D4512886466025A56F75A9A5DBEB32B8D609513E236"))
+(*local* "sdkpw" (ledger-get (*state* documents arxiv the-synchronic-web)))
